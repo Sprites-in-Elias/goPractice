badf371cb9fc8bee7433dc4ac6faa0bbb40b2219
opi6opi6 <okanekudasai6@proton.me>
2026-06-25 01:24:38 +0900

    TestEnv

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
     "log"
 	"os"
-    // "github.com/joho/godotenv"
+    "github.com/joho/godotenv"
 )
 
 
@@ -11,10 +11,10 @@ func main() {
 	log.Println("서버 시작!")
     
     // 1. .env 로드 시도
-    // err := godotenv.Load()
-    // if err != nil {
-    //     log.Println("경고: .env 파일을 찾을 수 없습니다 (서버 배포 환경이면 정상입니다)")
-    // }
+    err := godotenv.Load()
+    if err != nil {
+        log.Println("경고: .env 파일을 찾을 수 없습니다 (서버 배포 환경이면 정상입니다)")
+    }
 
     // 2. 환경 변수 읽기
     mongoURI := os.Getenv("MONGODB_URI")
